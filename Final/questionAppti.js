7d94d3a232f0b415427ef3a81ebc7c0b5836e846
Afrid1919 <132122733+Afrid1919@users.noreply.github.com>
2023-06-05 22:50:53 +0530

    Add files via upload

---
+++ Final/questionAppti.js
@@ -0,0 +1,109 @@
+let questionsVerbal = [
+    
+      {
+        numb: 3,
+        question: "The teacher asked if _____ to bring our textbooks to class.",
+        answer: "we had all remembered",        
+        
+        options: [
+          "all we had remembered",
+          "we had all remembered",                   
+          "had all we remembered",
+          "had we all remembered",
+        ],
+      },
+      {
+        numb: 4,
+        question: "I must ____ for being so late.",
+        answer: "pardon",      
+        options: [
+          "regret",
+          "apologise",
+          "excuse",
+          "pardon",
+        ],
+      },
+      {
+        numb: 5,
+        question: "I've only got a ____ money in the bank",
+        answer: "few",
+        options: [
+       
+         "less",
+         "little",
+         "few",
+         "bit",
+        ],
+      },
+      {
+        numb: 6,
+        question: " We _____ a lovely three weeks in the south of Spain last year.",
+        answer: "are",
+        options: [
+            
+          "are",
+            "do",
+            "have",
+            "will",
+          
+        ],
+      },
+      {
+        numb: 7,
+        question: "Would you like to _____ for shopping with me this afternoon?",
+        answer: "go",
+        options: [
+          
+          "go",
+          "do",
+          "make",
+          "get",
+         
+        ],
+      },
+      {
+        numb: 8,
+        question: "I'm very ____ could I have a glass of water ,please?",
+        answer: "thirsty",
+        options: [
+        
+         "empty",
+         "thirsty",//ans
+         "hungry",
+         "dirty",
+ 
+          
+         
+        ],
+      },
+
+      {
+        numb: 9,
+        question: "Would you like to _____ for shopping with me this afternoon?",
+        answer: "go",
+        options: [
+          
+          "go",
+          "do",
+          "make",
+          "get",
+         
+        ],
+      },
+
+      {
+        numb: 10,
+        question: "Would you like to _____ for shopping with me this afternoon?",
+        answer: "go",
+        options: [
+          
+          "go",
+          "do",
+          "make",
+          "get",
+         
+        ],
+      },
+
+
+]
---
+++ Final/questionVerbal.js
@@ -0,0 +1,130 @@
+let questionsVerbal = [
+    {
+      numb: 1,
+      question: " We _____ a lovely three weeks in the south of Spain last year.",
+      answer: "spent",
+      options: [
+        "took",
+        "did",
+        "spent",
+        "passed",
+      ],
+    },
+    {
+        numb: 2,
+        question:  "Please don't forget to message me when you _____ home.",
+        answer: "get",
+        options: [
+            "will get",
+            "get",
+            "are going to get",
+            "are getting",
+        ],
+      },
+      {
+        numb: 3,
+        question: "The teacher asked if _____ to bring our textbooks to class.",
+        answer: "we had all remembered",        
+        
+        options: [
+          "all we had remembered",
+          "we had all remembered",                   
+          "had all we remembered",
+          "had we all remembered",
+        ],
+      },
+      {
+        numb: 4,
+        question: "I must ____ for being so late.",
+        answer: "pardon",      
+        options: [
+          "regret",
+          "apologise",
+          "excuse",
+          "pardon",
+        ],
+      },
+      {
+        numb: 5,
+        question: "I've only got a ____ money in the bank",
+        answer: "few",
+        options: [
+       
+         "less",
+         "little",
+         "few",
+         "bit",
+        ],
+      },
+      {
+        numb: 6,
+        question: " We _____ a lovely three weeks in the south of Spain last year.",
+        answer: "are",
+        options: [
+            
+          "are",
+            "do",
+            "have",
+            "will",
+          
+        ],
+      },
+      {
+        numb: 7,
+        question: "Would you like to _____ for shopping with me this afternoon?",
+        answer: "go",
+        options: [
+          
+          "go",
+          "do",
+          "make",
+          "get",
+         
+        ],
+      },
+      {
+        numb: 8,
+        question: "I'm very ____ could I have a glass of water ,please?",
+        answer: "thirsty",
+        options: [
+        
+         "empty",
+         "thirsty",//ans
+         "hungry",
+         "dirty",
+ 
+          
+         
+        ],
+      },
+
+      {
+        numb: 9,
+        question: "Would you like to _____ for shopping with me this afternoon?",
+        answer: "go",
+        options: [
+          
+          "go",
+          "do",
+          "make",
+          "get",
+         
+        ],
+      },
+
+      {
+        numb: 10,
+        question: "Would you like to _____ for shopping with me this afternoon?",
+        answer: "go",
+        options: [
+          
+          "go",
+          "do",
+          "make",
+          "get",
+         
+        ],
+      },
+
+
+]
